c2c3e60731e125b278cde6a0f5c9566a8df74b97
Alibek <alibek.fras@gmail.com>
2023-09-14 21:03:59 +0600

    hosting partly done

--- js/language.js
+++ js/language.js
@@ -6,18 +6,18 @@ currentLanguageBtn.addEventListener("click", () => {
     languageList.style.display = languageList.style.display === "none" ? "block" : "none";
 });
 
-languageList.addEventListener("click", (event) => {
-    if (event.target.tagName === "A") {
-        currentLanguageBtn.textContent = event.target.textContent;
-        languageList.style.display = "none";
-    }
-});
+// languageList.addEventListener("click", (event) => {
+//     if (event.target.tagName === "A") {
+//         currentLanguageBtn.textContent = event.target.textContent;
+//         languageList.style.display = "none";
+//     }
+// });
 
-for (let i = 0; i < languageLi.length; i++) {
-    languageLi[i].addEventListener('click', () => {
-        currentLanguageBtn.classList.remove('kg')
-        currentLanguageBtn.classList.remove('ru')
-        currentLanguageBtn.classList.remove('eng')
-        currentLanguageBtn.classList.add(languageLi[i].innerText);
-    });
-};
+// for (let i = 0; i < languageLi.length; i++) {
+//     languageLi[i].addEventListener('click', () => {
+//         currentLanguageBtn.classList.remove('kg')
+//         currentLanguageBtn.classList.remove('ru')
+//         currentLanguageBtn.classList.remove('eng')
+//         currentLanguageBtn.classList.add(languageLi[i].innerText);
+//     });
+// };
